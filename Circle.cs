ea65831c718857da032fc64a4b5ee81ab8e701d2
Ivan Bondaruk <bondarukid@icloud.com>
2025-05-04 17:38:47 +0200

    Remove Circle implementation from OhmsLaw class to Circle class

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeC.Operations
+{
+    public static class Circle
+    {
+        public static double CircleRadius(double diameter)
+        {
+            return diameter / 2;
+        }
+
+        public static double CircleDiameter(double radius)
+        {
+            return radius * radius;
+        }
+
+        public static double CircleArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double CircleCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
--- OhmsLaw.cs
+++ OhmsLaw.cs
@@ -83,27 +83,4 @@ namespace WeC.Operations
 
         #endregion
     }
-
-    public static class Circle
-    {
-        public static double CircleRadius(double diameter)
-        {
-            return diameter / 2;
-        }
-
-        public static double CircleDiameter(double radius)
-        {
-            return radius * radius;
-        }
-
-        public static double CircleArea(double radius)
-        {
-            return Math.PI * Math.Pow(radius, 2);
-        }
-
-        public static double CircleCircumference(double radius)
-        {
-            return 2 * Math.PI * radius;
-        }
-    }
 }
