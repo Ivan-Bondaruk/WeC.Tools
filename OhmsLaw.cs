589d3f2bdbea62e763fe067ec5d0dfe0b2392ffe
Ivan Bondaruk <bondarukid@icloud.com>
2025-05-04 17:08:29 +0200

    First commit

---
+++ OhmsLaw.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Copyright (C) 2025 Ivan Bondaruk
+ * 
+ * This file is part of WeC.
+ * 
+ * It under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Author: Ivan Bondaruk
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeC.Operations
+{
+    public static class OhmsLaw
+    {
+        #region Ohms Law calculations vor voltage
+        public static double CalculateVRI(double resistance, double current)
+        {
+            return current * resistance;
+        }
+
+        public static double CalculateVPI(double power, double current)
+        {
+            return power / current;
+        }
+
+        public static double CalculateVPR(double power, double resistance)
+        {
+            return Math.Sqrt(power * resistance);
+        }
+        #endregion
+
+        #region Ohms Law calculations vor current
+
+        public static double CalculateIPR(double power, double resistance)
+        {
+            return Math.Sqrt(power / resistance);
+        }
+        public static double CalculateIPV(double power, double voltage)
+        {
+            return power / voltage;
+        }
+
+        public static double CalculateIVR(double voltage, double resistance)
+        {
+            return voltage / resistance;
+        }
+
+        #endregion
+
+        #region Ohms Law calculations vor resistance
+
+        public static double CalculateRVI(double voltage, double current)
+        {
+            return voltage / current;
+        }
+
+        public static double CalculateRVP(double voltage, double power)
+        {
+            return (Math.Pow(voltage, 2) / power);
+        }
+
+        public static double CalculateRPI(double power, double current)
+        {
+            return (power / Math.Pow(current, 2));
+        }
+
+        #endregion
+
+        #region Ohms Law calculations vor power
+
+        public static double CalculatePVI(double voltage, double current)
+        {
+            return voltage * current;
+        }
+
+        #endregion
+    }
+}
